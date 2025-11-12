53005c86b0ac92367ab6d8c6dd0c10b0b1675ea0
Khaled Eid <120110138+khaled-programer@users.noreply.github.com>
2025-11-12 22:44:59 +0200

    Create human and computer choice

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Rock  paper scissors game logic
+
+// create getComputerChoice function
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
+
+  if (choice === 0) {
+    return 'rock';
+  } else if (choice === 1) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
+}
+
+// create getHumanChoice function
+function getHumanChoice() {
+  return prompt('please enter your choice').toLocaleLowerCase();
+}
