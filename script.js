07cd4a7afbdb5fe88c6bb522fdbb4f55fcfbe1c7
Khaled Eid <120110138+khaled-programer@users.noreply.github.com>
2025-11-12 22:55:24 +0200

    Create playGame function

--- script.js
+++ script.js
@@ -18,35 +18,82 @@ function getHumanChoice() {
   return prompt('please enter your choice').toLocaleLowerCase();
 }
 
-let computerScore = 0;
-let humanScore = 0;
+// let computerScore = 0;
+// let humanScore = 0;
 
 // create playRound function
-function playRound(human, computer) {
-  human = getHumanChoice();
-  computer = getComputerChoice();
-  // game logic
-  if (human === computer) {
-    console.log('No winner No loser');
-  } else if (human === 'rock' && computer === 'paper') {
-    computerScore += 1;
-    console.log('You lose paper beats rock');
-  } else if (human === 'paper' && computer === 'rock') {
-    humanScore += 1;
-    console.log('you win paper beats rock');
-  } else if (human === 'rock' && computer === 'scissors') {
-    humanScore += 1;
-    console.log('you win rock beats scissors');
-  } else if (human === 'scissors' && computer === 'rock') {
-    computerScore += 1;
-    console.log('you lose rock beats scissors');
-  } else if (human === 'paper' && computer === 'scissors') {
-    computerScore += 1;
-    console.log('you lose scissors beats paper');
-  } else if (human === 'scissors' && computer === 'paper') {
-    humanScore += 1;
-    console.log('you win scissors beats paper');
+// function playRound(human, computer) {
+//   human = getHumanChoice();
+//   computer = getComputerChoice();
+//   // game logic
+//   if (human === computer) {
+//     console.log('No winner No loser');
+//   } else if (human === 'rock' && computer === 'paper') {
+//     computerScore += 1;
+//     console.log('You lose paper beats rock');
+//   } else if (human === 'paper' && computer === 'rock') {
+//     humanScore += 1;
+//     console.log('you win paper beats rock');
+//   } else if (human === 'rock' && computer === 'scissors') {
+//     humanScore += 1;
+//     console.log('you win rock beats scissors');
+//   } else if (human === 'scissors' && computer === 'rock') {
+//     computerScore += 1;
+//     console.log('you lose rock beats scissors');
+//   } else if (human === 'paper' && computer === 'scissors') {
+//     computerScore += 1;
+//     console.log('you lose scissors beats paper');
+//   } else if (human === 'scissors' && computer === 'paper') {
+//     humanScore += 1;
+//     console.log('you win scissors beats paper');
+//   }
+// }
+
+//playRound();
+
+// create playGame function
+function playGame() {
+  let computerScore = 0;
+  let humanScore = 0;
+
+  function playRound(human, computer) {
+    human = getHumanChoice();
+    computer = getComputerChoice();
+    // game logic
+    if (human === computer) {
+      console.log('No winner No loser');
+    } else if (human === 'rock' && computer === 'paper') {
+      computerScore += 1;
+      console.log('You lose paper beats rock');
+    } else if (human === 'paper' && computer === 'rock') {
+      humanScore += 1;
+      console.log('you win paper beats rock');
+    } else if (human === 'rock' && computer === 'scissors') {
+      humanScore += 1;
+      console.log('you win rock beats scissors');
+    } else if (human === 'scissors' && computer === 'rock') {
+      computerScore += 1;
+      console.log('you lose rock beats scissors');
+    } else if (human === 'paper' && computer === 'scissors') {
+      computerScore += 1;
+      console.log('you lose scissors beats paper');
+    } else if (human === 'scissors' && computer === 'paper') {
+      humanScore += 1;
+      console.log('you win scissors beats paper');
+    }
+  }
+
+  playRound()
+  playRound()
+  playRound()
+  playRound()
+  playRound()
+
+  if (humanScore > computerScore) {
+    console.log(`Final result Human Wins ${humanScore} To ${computerScore}`)
+  } else {
+    console.log(`Final result computer wins ${computerScore} To ${humanScore}`)
   }
 }
 
-//playRound();
+playGame();
